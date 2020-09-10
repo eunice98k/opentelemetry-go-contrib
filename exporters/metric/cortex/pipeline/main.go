7e0e46de89c080db168dc14f56de0b2a5a62583f
Eunice Kim <eunice98k@gmail.com>
2020-09-10 11:14:50 -0700

    revert back

--- exporters/metric/cortex/pipeline/main.go
+++ exporters/metric/cortex/pipeline/main.go
@@ -33,38 +33,38 @@ func main() {
 	// start := time.Now()
 
 	// // Run PipelineOne test.
-	fmt.Printf("[P1] Starting pipeline one test!\n\n")
-	fmt.Printf("[P1] Exporting data to Cortex!\n")
-	runPipelineOne("data/PrometheusDataFirst.csv", 1000)
+// 	fmt.Printf("[P1] Starting pipeline one test!\n\n")
+// 	fmt.Printf("[P1] Exporting data to Cortex!\n")
+// 	runPipelineOne("data/PrometheusDataFirst.csv", 1000)
 
-	fmt.Printf("\n[P1] Querying data from Cortex and writing results to disk!\n")
-	storePipelineOneResults("data/PrometheusDataFirst.csv", "data/pipelineOneResults.csv", 1000)
+// 	fmt.Printf("\n[P1] Querying data from Cortex and writing results to disk!\n")
+// 	storePipelineOneResults("data/PrometheusDataFirst.csv", "data/pipelineOneResults.csv", 1000)
 
-	fmt.Printf("\n[P1] Comparing the results and answers files!\n")
-	p1Valid := validatePipelineOne()
+// 	fmt.Printf("\n[P1] Comparing the results and answers files!\n")
+// 	p1Valid := validatePipelineOne()
 
-	// Run PipelineTwo test.
-	fmt.Printf("[P2] Starting pipeline two test!\n\n")
-	fmt.Printf("[P2] Exporting data to Cortex!\n")
-	runPipelineTwo()
+// 	// Run PipelineTwo test.
+// 	fmt.Printf("[P2] Starting pipeline two test!\n\n")
+// 	fmt.Printf("[P2] Exporting data to Cortex!\n")
+// 	runPipelineTwo()
 
-	fmt.Printf("\n[P2] Querying data from Cortex and writing results to disk!\n")
-	storePipelineTwoResults("data/PrometheusDataSecond.csv", "data/pipelineTwoResults.csv", 1000)
+// 	fmt.Printf("\n[P2] Querying data from Cortex and writing results to disk!\n")
+// 	storePipelineTwoResults("data/PrometheusDataSecond.csv", "data/pipelineTwoResults.csv", 1000)
 
-	fmt.Printf("\n[P2] Comparing the results and answers files!\n")
-	p2Valid := validatePipelineTwo()
+// 	fmt.Printf("\n[P2] Comparing the results and answers files!\n")
+// 	p2Valid := validatePipelineTwo()
 
-	// Print out elapsed time.
-	elapsed := time.Since(start)
+// 	// Print out elapsed time.
+// 	elapsed := time.Since(start)
 
-	fmt.Printf("\nCompleted pipeline tests!\n")
-	fmt.Printf("Elapsed Time: %v\n", elapsed)
-	if p1Valid {
-		fmt.Println("[Success] Pipeline One Validation Succeeded.")
-	}
-	if p2Valid {
-		fmt.Println("[Success] Pipeline Two Validation Succeeded.")
-	}
+// 	fmt.Printf("\nCompleted pipeline tests!\n")
+// 	fmt.Printf("Elapsed Time: %v\n", elapsed)
+// 	if p1Valid {
+// 		fmt.Println("[Success] Pipeline One Validation Succeeded.")
+// 	}
+// 	if p2Valid {
+// 		fmt.Println("[Success] Pipeline Two Validation Succeeded.")
+// 	}
 }
 
 // validatePipelineOne opens and compares the results and answers file for pipeline one.
